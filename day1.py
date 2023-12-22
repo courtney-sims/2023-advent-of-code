ae67c923bfc05b2a30b40e018924a87d333253f5
Courtney Sims <courtney.sims@wpengine.com>
2023-12-22 14:54:19 -0600

    day1 part2

--- day1.py
+++ day1.py
@@ -1,27 +1,27 @@
 def process_calibration_document(filepath):
-  sum = 0
-  with open (filepath, "r") as f:
-    for line in f:
-      sum += get_calibration_value(line)
-  return sum
+    sum = 0
+    with open(filepath, "r") as f:
+        for line in f:
+            sum += get_calibration_value(line)
+    return sum
+
 
 def get_calibration_value(line):
-  val=""
-  first=0
-  last=len(line)-1
+    first = 0
+    last = len(line)-1
 
-  while first < len(line)-1:
-    try:
-      int(line[first])
-      break
-    except:
-      first+=1
+    while first < len(line)-1:
+        try:
+            int(line[first])
+            break
+        except:
+            first += 1
 
-  while last > 0:
-    try:
-      int(line[last])
-      break
-    except:
-      last-=1
+    while last > 0:
+        try:
+            int(line[last])
+            break
+        except:
+            last -= 1
 
-  return int(line[first])*10+int(line[last])
+    return int(line[first])*10+int(line[last])
--- test_day1.py
+++ test_day1.py
@@ -2,22 +2,24 @@ import unittest
 
 import day1
 
+
 class TestDay1(unittest.TestCase):
-  def test_get_calibration_value(self):
-    testcases = [
-      {"input": "1abc2", "expected": 12},
-      {"input": "pqr3stu8vwx", "expected": 38},
-      {"input": "a1b2c3d4e5f", "expected": 15},
-      {"input": "treb7uchet", "expected": 77}
-    ]
+    def test_get_calibration_value(self):
+        testcases = [
+            {"input": "1abc2", "expected": 12},
+            {"input": "pqr3stu8vwx", "expected": 38},
+            {"input": "a1b2c3d4e5f", "expected": 15},
+            {"input": "treb7uchet", "expected": 77},
+        ]
 
-    for test in testcases:
-      val = day1.get_calibration_value(test["input"])
-      self.assertEqual(val, test["expected"])
+        for test in testcases:
+            val = day1.get_calibration_value(test["input"])
+            self.assertEqual(val, test["expected"])
 
-  def test_process_calibration_document(self):
-    val = day1.process_calibration_document("input-day1.txt")
-    self.assertEqual(val, 0)
+    def test_process_calibration_document(self):
+        val = day1.process_calibration_document("input.txt")
+        self.assertEqual(val, 55386)
+
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
